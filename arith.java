368ede86f104ed49844f57ca463da564691d2b6f
nagarajujangala-12 <245122749001@mvsrec.edu.in>
2025-03-25 15:57:30 +0530

    commit1

---
+++ arith.java
@@ -0,0 +1,16 @@
+public class arith
+{
+	public static void main(String args[])
+	{
+		int num1=9;
+		int num2=7;
+
+		System.out.println("Sum "+num1+num2);
+		int diff=num1-num2;
+		System.out.println("difference "+diff);
+		System.out.println("multiplication "+num1*num2);
+		System.out.println("division "+num1/num2);
+		System.out.println("remainder "+num1%num2);
+	}
+}
+		
